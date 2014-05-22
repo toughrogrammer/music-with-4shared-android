113d1ed8489f1b93d79ee0d4321b6e8b58ba4dd5
JungJuHong <junghong456@gmail.com>
2014-05-22 20:50:56 +0900

    view pager indicator 추가. 테마를 Action bar 없는걸로 변경.

--- app/src/main/java/my/app/free/musicloader/MainActivity.java
+++ app/src/main/java/my/app/free/musicloader/MainActivity.java
@@ -3,12 +3,15 @@ package my.app.free.musicloader;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v13.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.util.SparseArray;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.viewpagerindicator.TitlePageIndicator;
 
 import java.util.Locale;
 
@@ -59,6 +62,11 @@ public class MainActivity extends Activity implements OnNewItemDownload {
         mViewPager = (ViewPager) findViewById(R.id.pager);
         mViewPager.setAdapter(mSectionsPagerAdapter);
 
+        //Bind the title indicator to the adapter
+        TitlePageIndicator titleIndicator = (TitlePageIndicator)findViewById(R.id.titles);
+        titleIndicator.setViewPager(mViewPager);
+        titleIndicator.setTextColor(0xff000000);
+        titleIndicator.setSelectedColor(0xff000000);
     }
 
 
